ac12a9d3fb57ceccf0418054384e6ee237bc2d8f
Noah Perry <noah@perrybrain.com>
2022-09-14 19:25:39 -0500

    Initial commit

---
+++ src/main/java/dev/tehcn/infinityarmor/InfinityArmor.java
@@ -0,0 +1,15 @@
+package dev.tehcn.infinityarmor;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfinityArmor implements ModInitializer {
+    public static final String MOD_ID = "infinityarmor";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/tehcn/infinityarmor/InfinityArmorClient.java
@@ -0,0 +1,10 @@
+package dev.tehcn.infinityarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfinityArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
